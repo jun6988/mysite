6c881ead243ffc446570e92d332425dc8a9ccdcd
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-11-22 16:57:53 +0900

    gallery

---
+++ mysite03/src/main/java/com/bitacademy/mysite/service/GalleryService.java
@@ -0,0 +1,16 @@
+package com.bitacademy.mysite.service;
+
+import org.springframework.stereotype.Service;
+
+import com.bitacademy.mysite.vo.GalleryVo;
+
+@Service
+public class GalleryService {
+
+	public void saveImages(GalleryVo galleryVo) {
+	}
+
+	public void removeImages(Long no) {
+	}
+
+}
